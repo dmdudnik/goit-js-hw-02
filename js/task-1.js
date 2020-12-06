1352a5106893e2df33628dbd1f40bf55017d9e35
Dmitriy Dudnik <63814431+dmdudnik@users.noreply.github.com>
2020-12-06 21:45:38 +0200

    скрипт1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,10 @@
+const logItems = function(array) {
+    const logs = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
 
+    for (let i = 0; i < logs.length; i += 1) {
+        console.log(logs[i], length[i]);
+      }
+  };
+
+
+  logItems();
